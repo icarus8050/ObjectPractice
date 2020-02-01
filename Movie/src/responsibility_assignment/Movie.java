6c67dc52ac84d118d1eb02ab644e0221968a5f05
icarus8050 <icarus8050@naver.com>
2020-02-01 10:39:53 +0900

    다형성을 이용하여 Movie 의 할인 정책에 따른 역할을 분리

--- Movie/src/responsibility_assignment/Movie.java
+++ Movie/src/responsibility_assignment/Movie.java
@@ -1,13 +1,21 @@
 package responsibility_assignment;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 
-public class Movie {
+public abstract class Movie {
     private String title;
     private Duration runningTime;
     private Money fee;
     private List<DiscountCondition> discountConditions;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountConditions = Arrays.asList(discountConditions);
+    }
 
     public Money calculateMovieFee(Screening screening) {
         if (isDiscountable(screening)) {
@@ -20,5 +28,11 @@ public class Movie {
     private boolean isDiscountable(Screening screening) {
         return discountConditions.stream()
                 .anyMatch(condition -> condition.isSatisfiedBy(screening));
+    }
+
+    abstract protected Money calculateDiscountAmount();
+
+    protected Money getFee() {
+        return fee;
     }
 }
